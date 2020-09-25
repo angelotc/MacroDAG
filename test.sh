57b638fda7489f9f6e8820a80174a47f277e11c1
Angelo <angelocortez102@gmail.com>
2020-09-24 22:39:36 -0500

    remove useless shell files

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Build the base images from which are based the Dockerfiles
-# then Startup all the containers at once 
-python ~/Desktop/Projects/airflow-docker-hdfs-spark-example/mnt/airflow/dags/airflow_local_connections/airflow_connections.py
