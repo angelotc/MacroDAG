f8881f3cdbb80ab232ee43cde75b68932decc8b2
Angelo <angelocortez102@gmail.com>
2020-09-30 23:23:03 -0500

    start gdp ETL

--- mnt/airflow/dags/airflow_local_connections/airflow_connections.py
+++ mnt/airflow/dags/airflow_local_connections/airflow_connections.py
@@ -14,7 +14,9 @@ def add_connection( conn_id=None, conn_type=None, host=None, extra=None, login=N
 
 if __name__ == "__main__":
     add_connection('willshire_api', 'http', "https://fred.stlouisfed.org/graph/fredgraph.csv?mode=fred&recession_bars=on&ts=12&tts=12&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=WILL5000PRFC&scale=left&cosd=2019-06-02&coed=2020-09-04&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Daily%2C%20Close&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2020-09-08&revision_date=2020-09-08&nd=1999-12-31%22" )
+    add_connection('gdp_api', 'http', "https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=off&txtcolor=%23444444&ts=12&tts=12&width=968&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=GEPUCURRENT&scale=left&cosd=1997-01-01&coed=2020-07-01&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Monthly&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2020-09-30&revision_date=2020-09-30&nd=1997-01-01" )
     add_connection("willshire_path", "fs","", MY_CONFIG,"")
+    add_connection("gdp_path", "fs","", MY_CONFIG,"")
     add_connection( "spark_conn", "","spark://spark-master",  port=7077)
     add_connection( "hive_conn", "hiveserver2","hive-server", None,"hive","hive", 10000)
 
--- mnt/airflow/dags/willshire_dag.py
+++ mnt/airflow/dags/willshire_dag.py
@@ -26,7 +26,7 @@ default_args = {
     "retry_delay" : timedelta(minutes = 5) 
 }
 
-def download_rates():
+def download_willshire_rates():
     # SAVE URL -> CSV
     temp_file_name = 'willshire.csv'
     # 20 years ago
@@ -45,9 +45,28 @@ def download_rates():
         
         for row in reader:
             print(row)
-    # with open(os.path.join("/usr/local/airflow/dags/files/", "willshire.json"), encoding='utf-8') as outfile:
-    #     outfile.write(json.dumps(data, indent=4))
-    
+
+def download_gdp_monthly_rates():
+    # SAVE URL -> CSV
+    temp_file_name = 'gdp_quarterly.csv'
+    # 20 years ago
+    start_date = (datetime.now().date())
+    end_date = ((datetime.now() - timedelta(days=20*365)).date())
+    CSV_URL = 'https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=968&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=GDPC1&scale=left&cosd={}&coed={}&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Quarterly&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2020-09-30&revision_date=2020-09-30&nd=1947-01-01
+'.format(str(start_date),str(end_date))
+    data = {}
+    response = requests.get(CSV_URL)
+    with open(os.path.join("/usr/local/airflow/dags/files/", temp_file_name), 'wb') as f:
+        f.write(response.content)
+    f.close()
+
+    #Print URL
+    with open(os.path.join("/usr/local/airflow/dags/files/", temp_file_name), 'r') as readfile:
+        reader = csv.DictReader(readfile, delimiter=',')
+        
+        for row in reader:
+            print(row)
+
 
 
 with DAG(dag_id = "willshire_dag", 
@@ -64,11 +83,24 @@ with DAG(dag_id = "willshire_dag",
         poke_interval = 5,
         timeout=20
     )
-    
-    downloading_rates = PythonOperator(
-        task_id="downloading_rates",
-        python_callable=download_rates
+    is_gdp_rates_available = HttpSensor(
+        task_id = "is_willshire_5000_available",
+        method = "GET",
+        http_conn_id = "gdp_api",
+        endpoint = 'latest',
+        response_check = lambda response: "DATE" in response.text,
+        poke_interval = 5,
+        timeout=20
     )
+
+    downloading_willshire_rates = PythonOperator(
+        task_id="downloading_willshire_rates",
+        python_callable=download_willshire_rates
+    )
+    download_gdp_monthly_rates = PythonOperator(
+        task_id="download_gdp_monthly_rates",
+        python_callable=download_gdp_monthly_rates
+        
     is_willshire_file_available = FileSensor(
         task_id="is_willshire_file_available",
         fs_conn_id="willshire_path",
@@ -77,41 +109,81 @@ with DAG(dag_id = "willshire_dag",
         timeout=20
     )
 
+    is_gdp_file_available = FileSensor(
+        task_id="is_gdp_file_available",
+        fs_conn_id="gdp_path",
+        filepath="gdp_quarterly.csv",
+        poke_interval=5,
+        timeout=20
+    )
 
-    saving_rates = BashOperator(
-        task_id = "saving_rates",
+    saving_willshire_rates = BashOperator(
+        task_id = "saving_willshire_rates",
         bash_command = """
             hdfs dfs -mkdir -p /willshire && \
             hdfs dfs -put -f $AIRFLOW_HOME/dags/files/willshire.csv /willshire
         """
     )
+    saving_gdp_rates = BashOperator(
+        task_id = "saving_gdp_rates",
+        bash_command = """
+            hdfs dfs -mkdir -p /gdp_quarterly && \
+            hdfs dfs -put -f $AIRFLOW_HOME/dags/files/gdp_quarterly.csv /gdp_quarterly
+        """
+    )
     creating_willshire_base_table = HiveOperator(
-    task_id="creating_willshire_base_table",
-    hive_cli_conn_id="hive_conn",
-    hql="""
-        CREATE EXTERNAL TABLE IF NOT EXISTS willshire_base_table(
-            `date` DATE,
-            will5000 DOUBLE
+        task_id="creating_willshire_base_table",
+        hive_cli_conn_id="hive_conn",
+        hql="""
+            CREATE EXTERNAL TABLE IF NOT EXISTS willshire_base_table(
+                `date` DATE,
+                will5000 DOUBLE
+            )
+            ROW FORMAT DELIMITED
+            FIELDS TERMINATED BY ','
+            STORED AS TEXTFILE
+        """
         )
-        ROW FORMAT DELIMITED
-        FIELDS TERMINATED BY ','
-        STORED AS TEXTFILE
-    """
-    )
     creating_willshire_incremental_table = HiveOperator(
-    task_id="creating_willshire_incremental_table",
-    hive_cli_conn_id="hive_conn",
-    hql="""
-        CREATE EXTERNAL TABLE IF NOT EXISTS willshire_incremental_table(
-            `date` DATE,
-            will5000 DOUBLE
+        task_id="creating_willshire_incremental_table",
+        hive_cli_conn_id="hive_conn",
+        hql="""
+            CREATE EXTERNAL TABLE IF NOT EXISTS willshire_incremental_table(
+                `date` DATE,
+                will5000 DOUBLE
+            )
+            ROW FORMAT DELIMITED
+            FIELDS TERMINATED BY ','
+            STORED AS TEXTFILE
+        """
         )
-        ROW FORMAT DELIMITED
-        FIELDS TERMINATED BY ','
-        STORED AS TEXTFILE
-    """
-    )
 
+    creating_gdp_base_table = HiveOperator(
+        task_id="creating_gdp_base_table",
+        hive_cli_conn_id="hive_conn",
+        hql="""
+            CREATE EXTERNAL TABLE IF NOT EXISTS gdp_base_table(
+                `date` DATE,
+                gdp DOUBLE
+            )
+            ROW FORMAT DELIMITED
+            FIELDS TERMINATED BY ','
+            STORED AS TEXTFILE
+        """
+        )
+    creating_gdp_incremental_table = HiveOperator(
+        task_id="creating_gdp_incremental_table",
+        hive_cli_conn_id="hive_conn",
+        hql="""
+            CREATE EXTERNAL TABLE IF NOT EXISTS gdp_incremental_table(
+                `date` DATE,
+                gdp DOUBLE
+            )
+            ROW FORMAT DELIMITED
+            FIELDS TERMINATED BY ','
+            STORED AS TEXTFILE
+        """
+        )
 
     populate_willshire_incremental = SparkSubmitOperator(
         task_id = "populate_willshire_incremental",
@@ -127,12 +199,12 @@ with DAG(dag_id = "willshire_dag",
         verbose = False
     )
 
-    impute_willshire_zeros = SparkSubmitOperator(
-        task_id = "impute_willshire_zeros",
-        conn_id = "spark_conn",
-        application = "/usr/local/airflow/dags/scripts/impute_willshire_zeros.py",
-        verbose = False
-    )
+    # impute_willshire_zeros = SparkSubmitOperator(
+    #     task_id = "impute_willshire_zeros",
+    #     conn_id = "spark_conn",
+    #     application = "/usr/local/airflow/dags/scripts/impute_willshire_zeros.py",
+    #     verbose = False
+    # )
     delete_willshire_incremental_rows = SparkSubmitOperator(
         task_id = "delete_willshire_incremental_rows",
         conn_id = "spark_conn",
@@ -147,4 +219,6 @@ with DAG(dag_id = "willshire_dag",
     #         <h3>forex_data_pipeline succeeded</h3>
     #     """
     #     )
-    is_willshire_5000_available >> downloading_rates >> is_willshire_file_available >> saving_rates >> [creating_willshire_base_table, creating_willshire_incremental_table] >> populate_willshire_incremental >> populate_willshire_base >> delete_willshire_incremental_rows
+    is_willshire_5000_available >> downloading_willshire_rates >> is_willshire_file_available >> saving_willshire_rates 
+    saving_willshire_rates >> [creating_willshire_base_table, creating_willshire_incremental_table] >> populate_willshire_incremental 
+    populate_willshire_incremental >> populate_willshire_base >> delete_willshire_incremental_rows
